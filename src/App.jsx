d863f659290a159cb9bee452b388c3f22b6e1715
claireglowup <claireglowup@gmail.com>
2024-10-10 02:46:15 +0700

    ini submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,5 @@
 /** @format */
 
-import CoverItem from "./components/CoverItem";
-import FormAddProduct from "./components/FormAddProduct";
-import { getData } from "./utils/data";
-
-function App() {
-  const dataBoard = getData();
-
-  return (
-    <div className="list-item">
-      <FormAddProduct />
-      {dataBoard.map((item) => (
-        <CoverItem imageUrl={item.imageUrl} size={item.size} boardName={item.boardName} key={item.id} />
-      ))}
-    </div>
-  );
-}
+function App() {}
 
 export default App;
